8ac792410ced107c19f4fc79d306f5e71111241b
S. Fathima <fxthimx655@gmail.com>
2025-10-22 15:37:50 +0530

    Attendance tracking system

---
+++ class6.py
@@ -0,0 +1,16 @@
+attendance=[18,20,19,15,21]
+
+for x in attendance:
+   if x>= 20:
+    print("Full")
+   else:
+    print("Not Full")
+
+count=0
+for x in attendance:
+  if x>= 20:
+    count=count+1
+print("Days the class was full:",count)
+
+total_attendance= sum(attendance)
+print("Total attendance over 5 days:",total_attendance)
